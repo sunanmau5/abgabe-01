18e3aadf28edaac379de4351be79925598c5364f
Sunan Maunakea <72509654+sunanmau5@users.noreply.github.com>
2020-10-21 17:52:02 +0200

    Code documented

--- Assets/Scripts/FollowerScript.cs
+++ Assets/Scripts/FollowerScript.cs
@@ -6,19 +6,14 @@ public class FollowerScript : MonoBehaviour
 {
     [SerializeField]
     private Transform player = null;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        //direction = destination = source
+        // Direction = destination - source
         Vector3 directionToFace = player.position - transform.position;
 
-        //access our current rotation = Quaternion Look Rotation
+        // Access our current rotation = Quaternion Look Rotation
         transform.rotation = Quaternion.LookRotation(directionToFace);
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,27 +6,28 @@ public class PlayerMovement : MonoBehaviour
 {
     private float turnspeed = 100.0f;
     private float movespeed = 3.0f;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-        float moveVertical = Input.GetAxis("Vertical");
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        // Get the horizontal and vertical axis
+        float moveVertical = Input.GetAxis("Vertical") * movespeed;
+        float moveHorizontal = Input.GetAxis("Horizontal") * movespeed;
 
-        transform.Translate(0.0f, 0.0f, movespeed * moveVertical * Time.deltaTime);
-        transform.Translate(movespeed * moveHorizontal * Time.deltaTime, 0.0f, 0.0f);
+        // Move translation along the object's z-axis
+        transform.Translate(0.0f, 0.0f, moveVertical * Time.deltaTime);
+
+        // Move translation along the object's x-axis
+        transform.Translate(moveHorizontal * Time.deltaTime, 0.0f, 0.0f);
 
         if (Input.GetKey(KeyCode.E))
         {
+            // Rotate around the y-axis clockwise
             transform.Rotate(Vector3.up * turnspeed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.Q))
         {
+            // Rotate around the y-axis counter clockwise
             transform.Rotate(Vector3.down * turnspeed * Time.deltaTime);
         }
     }
