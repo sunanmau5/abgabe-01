b267d5245fbcd690653ed5f9db3220beff239ea6
sunanmau5 <s0566144@htw-berlin.de>
2020-10-29 00:28:29 +0100

    Added uncompleted Abgabe 02

---
+++ Assets/Scripts/MenuManagerScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManagerScript : MonoBehaviour
+{
+    public GameObject Menu;
+    public Button RestartButton;
+    public Button ReturnButton;
+    public static bool gameIsPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Menu.SetActive(false);
+        RestartButton.onClick.AddListener(Restart);
+        ReturnButton.onClick.AddListener(ResumeGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+    void PauseGame()
+    {
+        if (!gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            Menu.SetActive(true);
+            gameIsPaused = true;
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
+        Menu.SetActive(false);
+        gameIsPaused = false;
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        ResumeGame();
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,21 +14,24 @@ public class PlayerMovement : MonoBehaviour
         float moveVertical = Input.GetAxis("Vertical") * movespeed;
         float moveHorizontal = Input.GetAxis("Horizontal") * movespeed;
 
-        // Move translation along the object's z-axis
-        transform.Translate(0.0f, 0.0f, moveVertical * Time.deltaTime);
-
-        // Move translation along the object's x-axis
-        transform.Translate(moveHorizontal * Time.deltaTime, 0.0f, 0.0f);
-
-        if (Input.GetKey(KeyCode.E))
+        if (!MenuManagerScript.gameIsPaused)
         {
-            // Rotate around the y-axis clockwise
-            transform.Rotate(Vector3.up * turnspeed * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.Q))
-        {
-            // Rotate around the y-axis counter clockwise
-            transform.Rotate(Vector3.down * turnspeed * Time.deltaTime);
+            // Move translation along the object's z-axis
+            transform.Translate(0.0f, 0.0f, moveVertical * Time.deltaTime);
+
+            // Move translation along the object's x-axis
+            transform.Translate(moveHorizontal * Time.deltaTime, 0.0f, 0.0f);
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                // Rotate around the y-axis clockwise
+                transform.Rotate(transform.up * turnspeed * Time.deltaTime);
+            }
+            else if (Input.GetKey(KeyCode.Q))
+            {
+                // Rotate around the y-axis counter clockwise
+                transform.Rotate(-transform.up * turnspeed * Time.deltaTime);
+            }
         }
     }
 }
